032173cd7b2de51e629b08c03fe0821ddbaa0ee2
Muhammad  Nihal <muhammadnihal9421@gmail.com>
2023-10-12 11:10:09 -0400

    responsive website as a landing page

---
+++ script.js
@@ -0,0 +1,62 @@
+let nav = document.querySelector("#nav");
+setTimeout(function () {
+  nav.style.display = "flex";
+  setTimeout(function () {
+    nav.style.width = "310px";
+  }, 1000);
+}, 1000);
+
+var bgc4 = document.querySelector(".img-hidden");
+setTimeout(function () {
+  // bgc3.style.display = "block";
+  bgc4.style.width = "0px";
+  
+}, 3000);
+
+let page2 = document.querySelector("#page2");
+setTimeout(function () {
+  page2.style.width = "64%";
+  // setTimeout(function () {
+  //   page2.style.width = "0%";
+  // }, 5000);
+}, 1000);
+
+let smallDiv = document.querySelector("#small-div");
+setTimeout(function () {
+  smallDiv.style.width = "300px";
+}, 1000);
+
+// let textDiv = document.querySelector("#text-div > h1");
+// setTimeout(function () {
+//   textDiv.style.display = "block";
+// }, 1000);
+
+var bgc = document.querySelector(".hidden");
+setTimeout(function () {
+  // bgc.style.display = "block";
+  bgc.style.width = " 0";
+
+}, 2000);
+
+var bgc1 = document.querySelector(".hidden1");
+setTimeout(function () {
+  // bgc1.style.display = "block";
+  bgc1.style.width = "0px";
+
+  
+}, 3000);
+
+var bgc2 = document.querySelector(".hidden2");
+setTimeout(function () {
+  // bgc2.style.display = "block";
+  bgc2.style.width = "0px";
+
+}, 4000);
+
+var bgc3 = document.querySelector(".hidden3");
+setTimeout(function () {
+  // bgc3.style.display = "block";
+  bgc3.style.width = "0px";
+  
+}, 5000);
+
